ec9987c04a2f69c5cb9f6929f079e68fe2646073
Arkadiusz Matecki <arkadiusz.matecki@dazn.com>
2025-10-25 16:12:01 +0200

    Initial Commit

---
+++ Stories/StoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoriesApp.swift
+//  Stories
+//
+//  Created by Arkadiusz Matecki on 25/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct StoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StoriesTests/StoriesTests.swift
@@ -0,0 +1,17 @@
+//
+//  StoriesTests.swift
+//  StoriesTests
+//
+//  Created by Arkadiusz Matecki on 25/10/2025.
+//
+
+import Testing
+@testable import Stories
+
+struct StoriesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
